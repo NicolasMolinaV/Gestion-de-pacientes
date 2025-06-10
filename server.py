9f7564245c082cc241182816dd5cc93d6a952b05
NicolasMolinaV <nicola.molinav@duocuc.cl>
2025-06-09 21:31:36 -0400

    Fix routing issues

--- server.py
+++ server.py
@@ -2,19 +2,12 @@ import http.server
 import socketserver
 import os
 
-# Render define el puerto en una variable de entorno
 PORT = int(os.environ.get("PORT", 5500))
-DIRECTORY = os.path.join(os.path.dirname(__file__), "html")  # Ruta absoluta al directorio "html"
+DIRECTORY = "html"
 
-class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def translate_path(self, path):
-        # Sobreescribimos translate_path para que siempre busque en el directorio "html"
-        base_path = DIRECTORY
-        path = path.lstrip("/")
-        return os.path.join(base_path, path)
+os.chdir(DIRECTORY)
 
-# Crear servidor
-Handler = CustomHTTPRequestHandler
+Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Frontend corriendo en http://0.0.0.0:{PORT}")
